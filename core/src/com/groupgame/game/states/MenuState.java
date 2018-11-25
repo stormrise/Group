37a86c2f8e0477b50876c08edf5521adc4f1189e
stormrise <lilingxiao0414@gmail.com>
2018-11-25 10:49:02 +0800

    1

--- core/src/com/groupgame/game/states/MenuState.java
+++ core/src/com/groupgame/game/states/MenuState.java
@@ -47,7 +47,7 @@ public class MenuState extends State {
         sb.begin();
         sb.draw(background,0,0,GroupProject.WIDTH,GroupProject.HEIGHT);  //设置背景图位置
         sb.draw(butt,(GroupProject.WIDTH/2)-(butt.getWidth()/2),GroupProject.HEIGHT/2);
-        font.draw(sb,"!!!Welcome to Nijia Jump!!!",GroupProject.BRICK+20,GroupProject.HEIGHT/2+200);
+        font.draw(sb,"!!!Welcome to Ninja Jump!!!",GroupProject.BRICK+20,GroupProject.HEIGHT/2+200);
         font.setColor(Color.BLACK);
         font.getData().setScale(2.0f);
 
